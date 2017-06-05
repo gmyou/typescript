c81bc49ad52852c4b2d9a62ffd384514b8dc358b
Jack You <gmyou71@gmail.com>
2017-06-05 16:13:15 +0900

    sum matrix

--- variable-declarations/scope-rule.js
+++ variable-declarations/scope-rule.js
@@ -1,8 +1,15 @@
-function f(shouldInitialize) {
-    if (shouldInitialize) {
-        var x = 10;
+function sumMatrix(matrix) {
+    var sum = 0;
+    for (var i = 0; i < matrix.length; i++) {
+        var currentRow = matrix[i];
+        console.log(currentRow);
+        for (var i = 0; i < currentRow.length; i++) {
+            console.log(currentRow[i]);
+            sum += currentRow[i];
+        }
     }
-    return x;
+    return sum;
 }
-f(true); // returns '10'
-f(false); // returns 'undefined'
+var list = [[1, 2], [3, 4]];
+var result = sumMatrix(list);
+console.log(result); // 3
--- variable-declarations/scope-rule.ts
+++ variable-declarations/scope-rule.ts
@@ -1,10 +1,17 @@
-function f(shouldInitialize: boolean) {
-    if (shouldInitialize) {
-        var x = 10;
+function sumMatrix(matrix: number[][]) {
+    var sum = 0;
+    for (var i = 0; i < matrix.length; i++) {
+        var currentRow = matrix[i];
+        console.log(currentRow);
+        for (var i = 0; i < currentRow.length; i++) {
+            console.log(currentRow[i]);
+            sum += currentRow[i];
+        }
     }
 
-    return x;
+    return sum;
 }
 
-f(true);  // returns '10'
-f(false); // returns 'undefined'
+const list: number[][] = [[1, 2], [3, 4]];
+const result: number = sumMatrix(list);
+console.log(result);    // 3
