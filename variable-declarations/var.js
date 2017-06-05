c9203c295da09c894afd7417d65fe10d2126ce59
Jack You <gmyou71@gmail.com>
2017-06-05 15:58:30 +0900

    same variable

--- variable-declarations/var.js
+++ variable-declarations/var.js
@@ -1,6 +1,10 @@
-var a = 10;
 function f() {
-    var message = "Hello, world!";
-    return message;
+    var a = 10;
+    return function g() {
+        var b = a + 1;
+        return b;
+    }
 }
-f();
+
+var g = f();
+g();
