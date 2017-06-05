e2f8504a0af14cb8004040c875c23505879e830d
Jack You <gmyou71@gmail.com>
2017-06-05 16:36:58 +0900

    let

--- variable-declarations/capturing-quirks.ts
+++ variable-declarations/capturing-quirks.ts
@@ -5,3 +5,8 @@ for (var i = 0; i < 10; i++) {
         setTimeout(function() { console.log(i); }, 100 * i);
     })(i);
 }
+
+
+for (let i = 0; i < 10 ; i++) {
+    setTimeout(function() { console.log(i); }, 100 * i);
+}
