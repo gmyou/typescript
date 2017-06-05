0e80b416dda7305d1a25bfbba4f7cd05dab0d224
Jack You <gmyou71@gmail.com>
2017-06-05 16:43:35 +0900

    error

---
+++ variable-declarations/const.js
@@ -0,0 +1,15 @@
+var numLivesForCat = 9;
+var kitty = {
+    name: "Aurora",
+    numLives: numLivesForCat
+};
+// Error
+kitty = {
+    name: "Danielle",
+    numLives: numLivesForCat
+};
+// all "okay"
+kitty.name = "Rory";
+kitty.name = "Kitty";
+kitty.name = "Cat";
+kitty.numLives--;
